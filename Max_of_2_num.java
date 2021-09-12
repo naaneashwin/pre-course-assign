180541eb95b24352d4365177e9cea079f2877ec8
Ashwin Kumar R <ashwinrkumar56@gmail.com>
2021-09-12 17:42:41 +0530

    java precourse

---
+++ Max_of_2_num.java
@@ -0,0 +1,18 @@
+package Precourse;
+
+ class Max_of_2_num {
+
+	public static void main(String[] args) {
+		int x = 45;
+		int y = 66;
+		
+		if (x>y) {
+			System.out.println("x is greater");
+		}
+		else {
+			System.out.println("y is greater");
+		}
+
+	}
+
+}
---
+++ Max_of_2_num_using_ternary_opr.java
@@ -0,0 +1,13 @@
+package Precourse;
+
+public class Max_of_2_num_using_ternary_opr {
+
+	public static void main(String[] args) {
+		int x = 100;
+		int y = 20;
+		int great = (x>y)?x:y;
+		
+		System.out.println("The maximum value is "+great);
+	}
+
+}
